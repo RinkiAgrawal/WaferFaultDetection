1e3430894b37c30cc847ec588ae4b35f620b40ba
RinkiAgrawal <134221528+RinkiAgrawal@users.noreply.github.com>
2024-09-19 14:11:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+conda create -p env python==3.8 -y
+
+conda activate env/
+
+pip install -r requirements.txt
+
+ 
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="WaferFaultDetection",
+    version="0.0.1",
+    author="Rinki",
+    author_email="agrawalrinki816@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
